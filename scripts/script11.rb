fb615d5632ff50cf88436f5a2a783e5f9c1bdd3b
sergibyte <sergibyte@MacBook-SB.local>
2016-07-25 14:56:21 +0200

    Corrections to last script collect-each

--- scripts/script11.rb
+++ scripts/script11.rb
@@ -1,9 +1,9 @@
-#script10.rb
+#script11.rb
 require 'json'
 require 'date'
 file = File.open("apiresults.json", "rb")
 arrayofhashes = JSON.parse(file.read)["results"]
 
-arrayofhashes.collect { |thehash|  thehash["addedToDB"] = Time.now }
+arrayofhashes.each { |thehash|  thehash["addedToDB"] = Time.now }
 
 puts arrayofhashes.first
